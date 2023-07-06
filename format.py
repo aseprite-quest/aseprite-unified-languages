ee4062ccb17cdacc75fbf1df340408fc5b3bcfd2
TakWolf <takwolf@foxmail.com>
2023-07-06 23:18:56 +0800

    finish format script

--- format.py
+++ format.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from utils.aseini import Aseini
@@ -6,11 +7,24 @@ project_root_dir = os.path.dirname(__file__)
 strings_dir = os.path.join(project_root_dir, 'assets', 'strings')
 data_dir = os.path.join(project_root_dir, 'data')
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('format')
+
 
 def main():
+    logger.info(f"Load 'en' strings: 'main'")
     en_stings = Aseini.load(os.path.join(strings_dir, 'main', 'en.ini'))
-    en_stings.fallback(Aseini.load(os.path.join(strings_dir, '1.3-rc4', 'en.ini')))
-    en_stings.fallback(Aseini.load(os.path.join(strings_dir, '1.2.40', 'en.ini')))
+    for version in ['1.3-rc4', '1.2.40']:
+        logger.info(f"Fallback 'en' strings: '{version}'")
+        en_stings.fallback(Aseini.load(os.path.join(strings_dir, version, 'en.ini')))
+
+    for file_name in os.listdir(data_dir):
+        if not file_name.endswith('.ini'):
+            continue
+        file_path = os.path.join(data_dir, file_name)
+        logger.info(f"Format strings: '{file_name}'")
+        lang_strings = Aseini.load(file_path)
+        lang_strings.save(file_path, en_stings)
 
 
 if __name__ == '__main__':
